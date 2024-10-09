995fb35380ee901133e29e5b3a6e98f31b263460
u-shen <farah.ushen@gmail.com>
2024-10-09 16:37:24 +0100

    styled friends

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useFetchData } from "../../CustomHooks/useFetchData";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
+import GpsNotFixedIcon from "@mui/icons-material/GpsNotFixed";
+import ApartmentIcon from "@mui/icons-material/Apartment";
 import Friend1 from "./pix/friend-01.jpg";
 import Friend2 from "./pix/friend-02.jpg";
 import Friend3 from "./pix/friend-03.jpg";
@@ -12,8 +15,8 @@ import "./Friends.scss";
 function Friends() {
   const pix = [Friend1, Friend2, Friend3, Friend4, Friend5];
   const { data, isLoading, isPending, isError } = useFetchData();
-  // if (isLoading | isPending) return <h1>LOADING ...</h1>;
-  // if (isError) return <h1>ERROR ...</h1>;
+  if (isLoading | isPending) return <h1>LOADING ...</h1>;
+  if (isError) return <h1>ERROR ...</h1>;
   return (
     <section className="friends">
       <div className="friends-title">
@@ -39,6 +42,36 @@ function Friends() {
                   />
                 </div>
                 <h3>{user.name}</h3>
+                <small>{user.company.catchPhrase}</small>
+              </div>
+              <div className="desc-icons">
+                <div>
+                  <i>
+                    <GpsFixedIcon />
+                  </i>
+                  <p>{user.address.geo.lat}</p>
+                </div>
+                <div>
+                  <i>
+                    <GpsNotFixedIcon />
+                  </i>
+                  <p>{user.address.geo.lng}</p>
+                </div>
+                <div>
+                  <i>
+                    <ApartmentIcon />
+                  </i>
+                  <p>{user.address.city}</p>
+                </div>
+              </div>
+              <div className="meta-deta">
+                <div className="website">
+                  <small>{user.website}</small>
+                </div>
+                <div className="button">
+                  <button className="profile">Profile</button>
+                  <button className="remove">Remove</button>
+                </div>
               </div>
             </div>
           );
